6fa2b5dae23190de7c106aea88c093333a6c6e6d
unknown <omidmollaei2002@gmail.com>
2023-02-13 08:26:16 +0330

    define a dataclass to holds all the reserved tokens in one place and add it as a field to TrainingDataArgument dataclass

--- main.py
+++ main.py
@@ -4,58 +4,68 @@ Main script to train BERT. Run this script from command line with required argum
 
 
 import os
+import utils
+import tensorflow as tf
+from typing import Optional, Dict
 from dataclasses import dataclass, field
-from typing import Optional
-from arg_parser import MainArgumentParser
+
+
+@dataclass
+class _ReservedTokens:
+    padding: str = field(default="[PAD]")
+    unknown: str = field(default="[UNK]")
+    start: str = field(default="[START]")
+    end: str = field(default="[END]")
+    mask: str = field(default="[MASK]")
 
 
 @dataclass
 class ModelArguments:
     """ Bert config. The model architecture can be fully customized by these arguments. The default values are
     mostly same as original BERT Base model."""
-    model_name: str = field(
+    model_name: Optional[str] = field(
         default="model",
         metadata={
-            "help": "A Name for model",
+            "help": "A optional Name for model",
         }
     )
-    model_dim: int = field(
+    model_dim: Optional[int] = field(
         default=768,
         metadata={
             "help": "Model dimension (Embedding and positional embedding dimension too). "
         }
     )
-    num_attention_heads: int = field(
+    num_attention_heads: Optional[int] = field(
         default=12,
         metadata={
             "help": "Number of attention heads for each layers in model."
         }
     )
-    num_hidden_layers: int = field(
+    num_hidden_layers: Optional[int] = field(
         default=12,
         metadata={
             "help": "Number of hidden layers in model."
         }
     )
-    dropout_rate: float = field(
+    dropout_rate: Optional[float] = field(
         default=0.1,
         metadata={
             "help": "Dropout rate inside the model to avoid over-fitting."
         }
     )
-    activation: str = field(
+    activation: Optional[str] = field(
         default="relu",
         metadata={
             "help": "Activation function of hidden layers inside the model."
         }
     )
-    vocab_size: int = field(
+    vocab_size: Optional[int] = field(
         default=35_000,
         metadata={
             "help": "Size of input vocabulary to the model."
         }
     )
-    layer_norm_eps: float = field(
+    layer_norm_eps: Optional[float] = field(
         default=1e-6,
         metadata={
             "help": "Epsilon value of normalizer layer."
@@ -82,17 +92,42 @@ class TrainingDataArguments:
             "help": "Whether distinct lines of text in the dataset are to be handled as distinct sequences."
         }
     )
+    lower_case: bool = field(
+        default=False,
+        metadata={
+            "help": "Whether to lowercase the dataset or not"
+        }
+    )
+    cycle_length: Optional[int] = field(
+        default=5,
+        metadata={
+            "help": "The number of input elements that will be processed concurrently while reading text files."
+        }
+    )
+    tokenizer_saving_path: Optional[str] = field(
+        default=os.path.join("./", "tokenizer"),
+        metadata={
+            "help": "An optional path to save the tokenizer in. Default is in current path."
+        }
+    )
+    reserved_tokens: _ReservedTokens = field(
+        default=_ReservedTokens(),
+        metadata={
+            "help": "An instance of another dataclass which holds all the special tokens in one place."
+        }
+    )
 
 
 def main():
-    parser = MainArgumentParser((ModelArguments, TrainingDataArguments))
+    parser = utils.MainArgumentParser((ModelArguments, TrainingDataArguments))
     model_args, dataset_args = parser.parse_args_into_dataclasses()
-    print(model_args)
-    print()
-    print(dataset_args)
+    dataset_args.vocab_size = model_args.vocab_size
+
+    # -------------------------------
+    # ------ Build Tokenizer --------
+    # -------------------------------
+    tokenizer = utils.build_tokenizer(dataset_args)
 
 
 if __name__ == "__main__":
     main()
-
-
